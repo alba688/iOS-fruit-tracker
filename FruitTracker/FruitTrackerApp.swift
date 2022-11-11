62018549b412ff334fb8533b1e3a83e4f3f83c0d
Alexandra Baker <baker.alexandra88@gmail.com>
2022-11-11 10:40:54 +0100

    Initial Commit

---
+++ FruitTracker/FruitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitTrackerApp.swift
+//  FruitTracker
+//
+//  Created by Alexandra Baker on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
